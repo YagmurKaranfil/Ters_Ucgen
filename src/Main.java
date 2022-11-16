4b0f7b0b6cc5f08ec163d554bb31872549acfb06
YagmurKaranfil <59535304+YagmurKaranfil@users.noreply.github.com>
2022-11-16 18:32:34 +0300

    Ters_Ucgen

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Basamak Sayısı:");
+        int number=input.nextInt();
+
+        for(int i=0;i<number;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=2*number-1;k>=(2*i+1);k--){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
